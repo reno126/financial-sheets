1475ff66a9cd1d9ffd518f73d00c95a4a71a4050
Krzysztof Piekacz <krzysztof.piekacz@synlab.com>
2025-02-28 18:33:15 +0100

    Initial next configuration. Prettier added. Hello word page added.

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import './globals.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-emerald-950">
+        <main className="mx-auto max-w-5xl bg-emerald-50 p-4">{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export const metadata: Metadata = {
+  title: 'Financial sheets',
+  description: 'Testing some new front-end features based on simply UI app',
+};
+
