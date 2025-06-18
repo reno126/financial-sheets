dab55c7acbc520ead866a598cb05faf3b56ba7f7
Krzysztof Piekacz <krzysztof.piekacz@synlab.com>
2025-06-18 13:50:18 +0200

    Limitation usage info added. Nav menu extended

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-import { Stack } from '@mui/material';
+import { Paper, Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 export default function Page() {
   return (
     <Stack spacing={2}>
-      <Typography variant="h1" component="h1" className="mb-4">
+      <Typography variant="h2" component="h1" className="mb-4">
         This is Home Page.
       </Typography>
+      <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
+        <Typography variant="h4" component="h2">
+          Important usage limitation.
+        </Typography>
+        <Typography variant="body1" component="p">
+          Polygon provides a free API with a limited number of requests per minute. Therefore, an
+          overrun message will appear when using it.{' '}
+        </Typography>
+
+        <Typography variant="body1" component="p">
+          `Retry last action` button - will re-execute the query ending in an error Therefore you
+          need to wait before using this button.
+        </Typography>
+
+        <Typography variant="body1" component="p">
+          If you have scrolled through the list and retrieved a few more pages, then a limit error
+          will cause a SERIES of queries to be re-executed, which may cause another limit error.
+        </Typography>
+      </Paper>
       <Typography variant="h6" component="p">
         Please use nav menu.
       </Typography>
--- src/config/global.ts
+++ src/config/global.ts
@@ -1,6 +1,7 @@
 import { NavConfigProps } from '@/components/Navigation/NavBar';
 
 export const navigationConfig: NavConfigProps[] = [
+  { id: 'home', name: 'home', route: '/' },
   { id: 'tickers', name: 'Tickers table', route: 'tickers' },
   { id: 'contracts', name: 'Contracts table', route: 'contracts' },
 ];
